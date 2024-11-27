9c83ae2fea920da006c2fe46727804eb98bacb7c
petezahindustries <petezahindustries@gmail.com>
2024-11-27 17:13:11 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 
 self.__uv$config = {
-    prefix: '/static/ixl/',
-    bare:'https://ixl.lat/bare/',
+    prefix: '/static/brunys/ixl/work/',
+    bare:'https://static.99.135.156.178.clients.your-server.de/bare/',,
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
